ac1d17e1523a9ee615fc008820644436dfddb619
BugHunter-X <askzenai@gmail.com>
2025-01-19 12:50:05 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,10 @@
+function add(a: number, b: number): number {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both parameters must be numbers');
+  }
+  return a + b;
+}
+
+let result = add(1, 2); // This will work correctly
+
+let result2 = add(1, "2"); // This will throw an error at compile time.
